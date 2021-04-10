c90e2e16e4b6a8cdb013f082536b1b2f6c3a028f
Suner <SSyuleyman@academytrainee.com>
2021-04-10 18:30:04 +0000

    small letters

--- stack_run.sh
+++ stack_run.sh
@@ -3,5 +3,5 @@ scp -tt -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no docker-compose.yaml jenkins
 ssh -tt -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml OneCleverHorse-stack
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml onecleverhorsestack
 EOF
