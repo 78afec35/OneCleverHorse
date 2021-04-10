f47e78b74c849c20fb0b89b33f51867727cde9be
Suner <SSyuleyman@academytrainee.com>
2021-04-10 13:46:23 +0000

    trying to pass through build number

--- stack_run.sh
+++ stack_run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export app_version=$BUILD_NUMBER
-ssh swarm-manager
-set app_version=$BUILD_NUMBER
+ssh swarm-manager "~/tools/run_pvt.pl $BUILD_NUMBER"
 rm -rf OneCleverHorse/
 git clone https://github.com/78afec35/OneCleverHorse && cd OneCleverHorse
 pwd
