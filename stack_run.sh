671a65fe8c6fb49ad724f15de59b75cc6a3503c2
Suner <SSyuleyman@academytrainee.com>
2021-04-10 20:01:25 +0000

    trying to get docker to spin up instances

--- stack_run.sh
+++ stack_run.sh
@@ -12,9 +12,9 @@ ssh -tt -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no jenkins@swarm-manager << EO
     docker rmi -f
     docker rm -f
     docker ps
-    docker services ps
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     docker-compose --version
     docker stack deploy --compose-file /home/jenkins/OneCleverHorse/docker-compose.yaml onecleverhorsestack
+    docker stack services onecleverhorsestack
 EOF
