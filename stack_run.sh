26c59bae221f63f76c48c7ab1fd3f8246b65aaf8
Suner <SSyuleyman@academytrainee.com>
2021-04-10 14:14:23 +0000

    still trying to pass build number

--- stack_run.sh
+++ stack_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ssh swarm-manager "~/tools/run_pvt.pl $BUILD_NUMBER"
+ssh jenkins@swarm-manager "$BUILD_NUMBER"
 rm -rf OneCleverHorse/
 git clone https://github.com/78afec35/OneCleverHorse && cd OneCleverHorse
 pwd
 docker image prune -f -a
 docker-compose pull
 docker-compose build
-docker stack deploy -c docker-compose.yaml OneCleverHorse
+docker stack deploy -c stack-deploy.yaml OneCleverHorse
