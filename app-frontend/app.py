e49b6e66be1a4f565605023db86a61d48135c432
Suner <SSyuleyman@academytrainee.com>
2021-04-10 13:15:49 +0000

    exporting version number from jenkins to manager

--- app-frontend/app.py
+++ app-frontend/app.py
@@ -1,20 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
 # Replace [PASSWORD] with the root password for your mysql container
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql:3306/flask-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
 
 class Users(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	first_name = db.Column(db.String(30), nullable=False)
-	last_name = db.Column(db.String(30), nullable=False)
-	email = db.Column(db.String(150), nullable=False, unique=True)
-	def __repr__(self):
-		return ''.join(['User ID: ', str(self.id), '\r\n', 'Email: ', self.email, ' Name: ', self.first_name, ' ', self.last_name, '\n'])
-
+	username = db.Column(db.String(30), nullable=False)
+	password = db.Column(db.String(30), nullable=False)
 
 @app.route('/')
 def hello():
--- stack_run.sh
+++ stack_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+export app_version=$BUILD_NUMBER
 ssh swarm-manager
+set app_version=$BUILD_NUMBER
 rm -rf OneCleverHorse/
 git clone https://github.com/78afec35/OneCleverHorse && cd OneCleverHorse
 pwd
