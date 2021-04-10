92bc2578be3023eb1df1bc16d3630dc624e982cc
Suner <SSyuleyman@academytrainee.com>
2021-04-10 12:57:34 +0000

    asking for more outputs to help troubleshoot

--- stack_run.sh
+++ stack_run.sh
@@ -2,6 +2,7 @@
 ssh swarm-manager
 rm -rf OneCleverHorse/
 git clone https://github.com/78afec35/OneCleverHorse && cd OneCleverHorse
+pwd
 docker image prune -f -a
 docker-compose pull
 docker-compose build
