2c95f5ddb19507ad71a7e726fba4ac5663fad6d0
Suner <SSyuleyman@academytrainee.com>
2021-04-10 18:07:36 +0000

    making sure environment variables aren't interferi

--- stack_run.sh
+++ stack_run.sh
@@ -3,6 +3,5 @@ scp -tt -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no docker-compose.yaml jenkins
 ssh -tt -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
-    export BUILD_NUMBER=${BUILD_NUMBER}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml OneCleverHorse-stack
 EOF
