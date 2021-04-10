699dff3e2c702656f8e6c240020f90135699bb0b
Suner <SSyuleyman@academytrainee.com>
2021-04-10 14:42:42 +0000

    exporting variables

--- stack_run.sh
+++ stack_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export BUILD_NUMBER = $BUILD_NUMBER
+export DATABASE_URI = $DATABASE_URI
 ssh jenkins@swarm-manager "$BUILD_NUMBER"
 rm -rf OneCleverHorse/
 git clone https://github.com/78afec35/OneCleverHorse && cd OneCleverHorse
