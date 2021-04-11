8c4a67758703f0c3fe1ba2ed036e32ec5c4e8125
Suner <SSyuleyman@academytrainee.com>
2021-04-11 18:55:51 +0000

    fixed syntax error for frontend

--- app-frontend/app.py
+++ app-frontend/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	rcomb = request.get('http://localhost:5003/rcomb')
-    return render_template=(index.html, rcomb=rcomb)
+    return render_template=('index.html', rcomb=rcomb)
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
