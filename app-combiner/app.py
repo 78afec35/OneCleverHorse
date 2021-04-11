0b60113fc95ec30fee36eedf7031c5156c6f65d7
Suner <SSyuleyman@academytrainee.com>
2021-04-11 21:00:55 +0000

    stringifying things

--- app-combiner/app.py
+++ app-combiner/app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 @app.route('/comb', methods=['GET', 'POST'])
 def comb():
-    adj1 = request.args.get('http://localhost:5001/adj')
-    adj2 = request.args.get('http://localhost:5001/adj')
-    noun1 = request.args.get('http://localhost:5002/noun')
-    noun2 = request.args.get('http://localhost:5002/noun')
-    rcomb = txt.title(adj1)+txt.title(noun1)+txt.title(adj2)+txt.title(noun2)
+    adj1 = request.get('http://localhost:5001/adj')
+    adj2 = request.get('http://localhost:5001/adj')
+    noun1 = request.get('http://localhost:5002/noun')
+    noun2 = request.get('http://localhost:5002/noun')
+    rcomb = txt.title(str(adj1))+txt.title(str(noun1))+txt.title(str(adj2))+txt.title(str(noun2))
     return Responce( str(rcomb), mimetype='text/plain' )
 
 
