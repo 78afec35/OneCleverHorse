194d7973b5bc5138e88e4e472746156ac3e9493a
Suner <SSyuleyman@academytrainee.com>
2021-04-12 11:31:13 +0000

    making sure requests is installed

--- stack_run.sh
+++ stack_run.sh
@@ -15,6 +15,8 @@ ssh -tt -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no jenkins@swarm-manager << EO
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     docker-compose --version
+    sudo apt install python3 python3-pip
+    sudo pip3 install requests
     docker stack deploy --compose-file /home/jenkins/OneCleverHorse/docker-compose.yaml onecleverhorsestack
     docker stack services onecleverhorsestack
     docker service scale onecleverhorsestack_appnoun=6
