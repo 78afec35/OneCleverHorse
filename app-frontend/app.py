7bff13f652f07c69dd5017faf475b07808736d03
Suner <SSyuleyman@academytrainee.com>
2021-04-12 12:08:05 +0000

    making sure everything is stringified.

--- app-frontend/app.py
+++ app-frontend/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     rcomb = request.get('http://localhost:5003/rcomb')
-    return render_template('index.html', rcomb=rcomb.text)
+    return render_template('index.html', rcomb=str(rcomb))
 
 
 if __name__=='__main__':
